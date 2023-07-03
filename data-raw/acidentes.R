5aac8110868dc4706558ce21fcfa02c1865b2bfb
pabsantos <pabsantos21@gmail.com>
2023-07-03 16:29:51 -0300

    Load "acidentes" and "vitimas"

--- data-raw/acidentes.R
+++ data-raw/acidentes.R
@@ -1,3 +1,17 @@
-## code to prepare `acidentes` dataset goes here
+temp1 <- tempfile()
+temp2 <- tempfile()
 
-usethis::use_data(acidentes, overwrite = TRUE)
+download.file(
+  url = "http://dados.transportes.gov.br/dataset/42e2320b-ea67-4fdc-896f-71363e043fc6/resource/f76bcc13-4bce-4e15-af42-38bdbce47da5/download/renaest_dabertos_20230512.zip",
+  destfile = temp1,
+  method = "wget"
+)
+
+unzip(zipfile = temp1, exdir = temp2)
+
+acidentes <- readr::read_csv2(paste0(
+  temp2,
+  "/Acidentes_DadosAbertos_20230512.csv"
+))
+
+save(acidentes, file = "data/acidentes.rda", compress = "bzip2")
