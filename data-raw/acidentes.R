67b7380f11fe70fd3170199859da6c110cdb61b0
Marco Guibor <106402555+marcoaguibor@users.noreply.github.com>
2023-09-21 17:01:06 -0300

    Update acidentes.R

--- data-raw/acidentes.R
+++ data-raw/acidentes.R
@@ -4,7 +4,7 @@ temp1 <- tempfile()
 temp2 <- tempfile()
 
 download.file(
-  url = "http://dados.transportes.gov.br/dataset/42e2320b-ea67-4fdc-896f-71363e043fc6/resource/f76bcc13-4bce-4e15-af42-38bdbce47da5/download/renaest_dabertos_20230512.zip",
+  url = "http://dados.transportes.gov.br/dataset/42e2320b-ea67-4fdc-896f-71363e043fc6/resource/dd6aadae-7655-4fb8-b537-5a4e3e42d28e/download/renaest_dabertos_20230912.zip",
   destfile = temp1,
   method = "wget"
 )
